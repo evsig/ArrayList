3a48a93ebab51d30eb816bb26dcdf67d36ffeb18
evsigneev <evsigneev.a@mail.ru>
2020-03-02 16:19:56 +0300

    add tests

---
+++ ArrayList.Tests/ArrayList.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ArrayList.Tests
+{
+    class ArrayListTests
+    {
+        [TestCase(new int[] { },0)]
+        [TestCase(new int[] {1},1)]
+        [TestCase(new int[] {1,2,3},3)]
+
+        public void SizeTest(int[] original, int expected)
+        {
+            ArrayList list = new ArrayList(original);
+            Assert.AreEqual(expected, list.Size());
+        }
+
+    }
+}
